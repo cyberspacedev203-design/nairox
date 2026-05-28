4930293c3b2bf9444021e584a0701fb1274b12f4
cyberspacedev203-design <cyberspace.dev203@gmail.com>
2026-05-28 11:38:06 +0100

    Update GatewayActivation.tsx

--- src/pages/GatewayActivation.tsx
+++ src/pages/GatewayActivation.tsx
@@ -111,11 +111,11 @@ const GatewayActivation = () => {
               <p className="text-sm font-semibold">Bank Details</p>
               <div className="space-y-1 text-sm">
                 <div className="flex items-center justify-between">
-                  <p className="font-mono">Account: 1097436921</p>
-                  <CopyButton text="1097436921" />
+                  <p className="font-mono">Account: 2065669422</p>
+                  <CopyButton text="2065669422" />
                 </div>
                 <p>Name: David Odum</p>
-                <p>Bank: paga bank</p>
+                <p>Bank: kuda bank</p>
               </div>
             </div>
             <NarrationSection text="Activation fee" />
